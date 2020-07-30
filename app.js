7a420fecbcb08dde7433c865fc3b54b05a719cf5
jebnascimento <eduardonascimento@poli.ufrj.br>
2020-07-30 19:07:09 -0300

    add twit, request, mime and bluebird dependencies packages

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require('express');
+const twit = require("twit")
 const port = 3000;
 const app = express();
 
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-    res.render('index');
+    res.render('index', {welcomeMessage: "Bem-vindo lindão"});
 });
 
 
