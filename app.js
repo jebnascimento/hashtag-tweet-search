4fb238ca3a847123cbe2a5c999df92948dac09f1
jebnascimento <eduardonascimento@poli.ufrj.br>
2020-07-30 16:13:30 -0300

    add views folder

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ const express = require('express');
 const port = 3000;
 const app = express();
 
-app.get('/', (req, res, next) => {
-    res.send({
-        title: "Meu primeiro servidor Express",
-        version: "0.0.0"
-    });
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.render('index');
 });
+
 
 app.listen(port, err => {
     console.log(`Server is listening on ${port}`);
