8f39100199e5c9d7c14c971599cdcd3d4b4c8ceb
jebnascimento <eduardonascimento@poli.ufrj.br>
2020-07-30 19:23:43 -0300

    test tweeter api with basic get method

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 const express = require('express');
-const twit = require("twit")
+const twit = require("twit");
+var config = require('./config.js');  
 const port = 3000;
 const app = express();
+
 
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
+    var T = new twit(config);
+
+    T.get('search/tweets', {
+        q: '#100DaysOfCode',
+        count: 10,
+        result_type: "mixed" 
+    }).catch(function (err) {
+        console.log('caught error', err.stack)
+    }).then(function (result) {
+        console.log('data', result.data);
+    });
     res.render('index', {welcomeMessage: "Bem-vindo lindão"});
 });
 
