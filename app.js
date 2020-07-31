b12ace8f830cff2061bb4bfc0f22f300a9b1a385
jebnascimento <eduardonascimento@poli.ufrj.br>
2020-07-31 15:04:02 -0300

    create post request to handle the form submit to get tweets by hashtag

--- app.js
+++ app.js
@@ -2,41 +2,62 @@ const express = require('express');
 const twit = require("twit");
 const bodyParser = require('body-parser')
 var config = require('./config.js');  
-const port = 8000;
+const port = 3000;
+
 const app = express();
 
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 
+
 app.get('/', (req, res) => {
-
-    var Twitter = new twit(config);
-
-    Twitter.get('search/tweets', {
-        q: '#android',
-        count: 10,
-        result_type: "mixed",
-        lang: "pt" 
-    }).catch(function (err) {
-        console.log('caught error', err.stack)
-    }).then(function (result) {
-        console.log('data', result.data);
-    });
-    res.render('index');
+    res.render('index', {hashtag: null, tweets: null})
 });
 
-app.post('/', function (req, res) {
-    console.log(req.body.hashtag);
-    if (req.body.hashtag !== undefined) {
-      res.render('index',  {hashtag: req.body.hashtag})
-    }
-    res.render('index',  {hashtag: null})
-    
-  });
+// app.get('/location', (req, res) => {
+//     let Twitter = new twit(config);
+//     Twitter.get('geo/search', {
+//     query: 'brazil'
+//     }).catch(function (err) {
+//     console.log('caught error', err.stack)
+//     }).then(function (result) {
+//         console.log('data', result.data);
+//     });
+// });
 
+
+app.post('/', function (req, res) {
+
+    if (req.body.hashtag !== null) {
+  
+        let Twitter = new twit(config);
+  
+        Twitter.get('search/tweets', {
+            q: req.body.hashtag, // use the user posted hashtag value as the query
+            count: 30,
+            result_type: "recent",
+            lang: "pt"
+    
+        }).catch(function (err) {
+            console.log('caught error', err.stack)
+            res.render('index', {
+                hashtag: null,
+                tweets: null,
+                error: err.stack
+            });
+        }).then(function (result) {
+            // Render the index page passing in the hashtag and the Twitter API results
+            res.render('index', {
+                hashtag: req.body.hashtag, 
+                tweets: result.data,
+                error: null
+            });
+        });
+    }
+  });
 
 app.listen(port, err => {
     console.log(`Server is listening on ${port}`);
 });
-
+    
