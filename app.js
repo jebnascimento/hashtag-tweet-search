d67046621d1be7059984f251d52f6ee34cdf14c0
jebnascimento <eduardonascimento@poli.ufrj.br>
2020-08-01 22:54:02 -0300

    refactor: edit app.js and README

--- app.js
+++ app.js
@@ -18,6 +18,6 @@ app.post('/tweets', tweetRoute);
 
 var port = process.env.port || 3000;
 app.listen(port, err => {
-    console.log(`Server is listening on ${port}`);
+    console.log(`Server is listening on port ${port}`);
 });
     
