49887ae3b8f4d262ebb9498d159515330e8b2f84
Eduardo Nascimento <eduardonascimento@poli.ufrj.br>
2020-08-06 19:39:55 -0300

    removing auth information

--- configs/twitterAuth.js
+++ configs/twitterAuth.js
@@ -1,6 +1,6 @@
 module.exports = {
-  consumer_key:         'NvbaKOOE0wuXfd1JFNwruTrG5',
-  consumer_secret:      'Ufyie4Smokdf4JIpZlSEY1dmjyN3nlxK60G80YuAeM8rJI70GB',
-  access_token:         '53285676-z8vRH3lnjoBuBmb9MbrPz2CVVfYIVeJzqBONPgcI3',
-  access_token_secret:  '17nVyAxXnDx3DzpNPMorkFjNb17V9zgUjl0z2RobhajX5'
+  consumer_key:         '',
+  consumer_secret:      '',
+  access_token:         '',
+  access_token_secret:  ''
 }
